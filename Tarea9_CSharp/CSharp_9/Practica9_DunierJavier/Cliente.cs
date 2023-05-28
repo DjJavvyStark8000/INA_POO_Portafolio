76d7063c03ff2cf445c56c2c114fc04f25a7f003
Javier B Ramírez <603140298@ina.cr>
2023-05-28 10:19:27 -0600

    Actualizacion de archivos tarea 9

---
+++ Tarea9_CSharp/CSharp_9/Practica9_DunierJavier/Cliente.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portafolio9_DunierJavier
+{
+    internal class Cliente
+    {
+        private int idCliente;
+        private string? nombre;
+        private string? apellido1;
+        private string? fechaNacimiento;
+        private int telefono;
+        private string? direccion;
+
+        //constructors
+        public Cliente()
+        {
+            idCliente = 0;
+            nombre = string.Empty;
+            apellido1 = string.Empty;
+            fechaNacimiento = string.Empty;
+            telefono = 00000000;
+            direccion = string.Empty;
+        }
+
+        public Cliente(int telefono)
+        {
+            this.telefono = telefono;
+        }
+
+        public Cliente(string nombre, string apellido1)
+        {
+            this.nombre = nombre;
+            this.apellido1 = apellido1;
+        }
+
+
+        //setters y getters mas simplificados en esta ocacion.
+        public int IdCliente { get; set; }
+        public string? Nombre { get; set; }
+        public string? Apellido1 { get; set; }
+        public string? FechaNacimiento { get; set; }
+        public int Telefono { get; set; }
+        public string? Direccion { get; set; }
+
+        //metodos get
+        public string getNombreApell()
+        {
+            string result = $"Nombre: {nombre}\nApellido: {apellido1}";
+            return result;
+        }
+
+        public string getNombreId()
+        {
+            string result = $"Nombre: {nombre}\nIdentificador: {idCliente}";
+            return result;
+        }
+
+        //metodos set
+        public void setTelefono(int telefono)
+        {
+            this.telefono = telefono;
+        }
+
+        public void setNombreApellido(string nombre, string apellido1)
+        {
+            this.nombre = nombre;
+            this.apellido1 = apellido1;
+        }
+
+        //funcionalidades y sobrecarga de metodos
+        public void printClientInfo()
+        {
+            Console.WriteLine($"Identificador: {idCliente}\n" +
+                $"Nombre: {nombre}\n" +
+                $"Apellido1: {apellido1}\n" +
+                $"Nacimiento: {fechaNacimiento}\n" +
+                $"Telefono: {telefono}\n" +
+                $"Direccion: {direccion}");
+        }
+
+        public bool checkPhone(int telefono)
+        {
+            bool check = false;
+            if (telefono > 22222222 && telefono <= 99999999)
+            {
+                check = true;
+            }
+            return check;
+        }
+
+        public void printClientInfo(string nombre)
+        {
+            
+            if (nombre[0].ToString().ToLower() == "a" || nombre[0].ToString().ToLower() == "b" ||
+                nombre[0].ToString().ToLower() == "c")
+            {
+                Console.WriteLine("Participantes del sorteo de un libro");
+            }
+            else if(nombre[0].ToString().ToLower() == "x" || nombre[0].ToString().ToLower() == "y" ||
+                nombre[0].ToString().ToLower() == "z")
+            {
+                Console.WriteLine("Participantes del sorteo de dos libros");
+            }else
+            {
+                Console.WriteLine("Participan el proximo mes");
+            }
+        }
+
+
+
+
+    }
+}
---
+++ Tarea9_CSharp/CSharp_9/Practica9_DunierJavier/Libro.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portafolio9_DunierJavier
+{
+    internal class Libro
+    {
+        private string? titulo;
+        private string? autor;
+        private int? anioPublicacion;
+        private string? editorial;
+        private string? categoria;
+        private double? precio;
+
+        //constructor
+        public Libro()
+        {
+            titulo = string.Empty;
+            autor = string.Empty;
+            anioPublicacion = 0;
+            editorial = string.Empty;
+            categoria = string.Empty;
+            precio = 0;
+        }
+
+        //aqui puede hacer un constructor con parametros (SOBRECARGAS)
+        public Libro(string titulo, string editorial)
+        {
+            this.titulo = titulo;
+            this.editorial = editorial;
+        }
+
+        public Libro(double precio)
+        {
+            this.precio = precio;
+        }
+        //setter y getters
+        public string? Titulo
+        {
+            get => titulo;
+            set { titulo = value; }
+        }
+
+        public string? Autor
+        {
+            get => autor;
+            set { autor = value; }
+        }
+
+        public int? AnioPublicacion
+        {
+            get => anioPublicacion;
+            set { anioPublicacion = value; }
+        }
+
+        public string? Editorial
+        {
+            get => editorial;
+            set { editorial = value; }
+        }
+
+        public string? Categoria
+        {
+            get => categoria;
+            set { categoria = value; }
+        }
+        public double? Precio
+        {
+            get => precio;
+            set { precio = value; }
+        }
+
+        //metodos get
+        public string getAutorLibro()
+        {
+            string result = $"Autor: {autor}\nLibro: {titulo}";
+            return result;
+        }
+
+        public string getBookPrice()
+        {
+            string result = $"Titulo: {titulo}\nPrecio: {precio}";
+            return result;
+        }
+
+        //metodos set
+        public void setPrice(double precio)
+        {
+            this.precio = precio;
+        }
+
+        public void setEditorial(string titulo, string editorial)
+        {
+            this.titulo = titulo;
+            this.editorial = editorial;
+        }
+
+
+        //metodos con funcionalidad y sobrecargas
+        public void printInfo()
+        {
+            Console.WriteLine(
+                "Datos generales del libro:\n" +
+                $"Titulo: {titulo}\n" +
+                $"Autor: {autor}\n" +
+                $"Anio: {anioPublicacion}\n" +
+                $"Editorial: {editorial}\n" +
+                $"Categoria: {categoria}\n" +
+                $"Precio: {precio}");
+        }
+
+        public void printInfo(int anioPublicacion)
+        {
+            if (anioPublicacion > DateTime.Now.Year)
+            {
+                Console.WriteLine("Anio fuera de rango");
+            }
+            else if (anioPublicacion <= DateTime.Now.Year && anioPublicacion > 2000)
+            {
+                Console.WriteLine("Existe una gran variedad de libros relativamente modernos");
+            }
+            else
+            {
+                Console.WriteLine("Existe una gran variedad de libros antiguos a la disposicion :)");
+            }
+        }
+
+        public double printInfo(string nombre, int precio)
+        {
+            double descuento = 0.0;
+            DateTime fechaActual = DateTime.Now;
+            int esteMes = fechaActual.Month;
+
+            if (esteMes == 4 || esteMes == 8)
+            {
+                descuento = precio * 0.5;
+            }
+
+            double precioFinal = precio - descuento;
+
+            return precioFinal;
+        }
+    }
+}
